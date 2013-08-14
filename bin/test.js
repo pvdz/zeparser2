25b6b563d07dcb5d43ca4eef7aafb988252a4420
Peter van der Zee <peter+zee@surfly.com>
2013-08-15 01:30:53 +0200

    Cli file update

---
+++ bin/test.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// echo "foo" | cli.js
+var Par = require(__dirname+'/../src/par.js').Par;
+
+var data = '';
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (chunk) {
+  data += chunk;
+});
+
+process.stdin.on('end', function () {
+  var par = new Par(data);
+  par.run();
+  console.log('-- finished');
+});
+
